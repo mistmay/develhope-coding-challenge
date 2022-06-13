0384cec104fe067c99ea1f9d6046c365e3fbc9fd
Giovanni <giovy.pod.da99@outlook.it>
2022-06-13 18:00:01 +0200

    Genera attivita e salva preferito

--- src/app/models/activity.ts
+++ src/app/models/activity.ts
@@ -1,7 +1,7 @@
 export interface Activity {
     activity: string;
     type: string;
-    partecipants: number;
+    participants: number;
     price: number;
     link: string;
     key: string;
--- src/app/modules/activity/generate-activity/generate-activity.component.ts
+++ src/app/modules/activity/generate-activity/generate-activity.component.ts
@@ -1,28 +1,54 @@
+import { Subscription } from 'rxjs';
+import { Activity } from 'src/app/models/activity';
 import { ApiService } from './../../../api/api.service';
 
 import { ActivityService } from './../../../services/activity.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-generate-activity',
   templateUrl: './generate-activity.component.html',
   styleUrls: ['./generate-activity.component.scss']
 })
-export class GenerateActivityComponent implements OnInit {
-
+export class GenerateActivityComponent implements OnInit, OnDestroy {
+  activities!: Activity[];
+  subscriptions: Subscription[] = [];
+  currentActivity!: Activity; 
+  btnDisable:boolean = false;
   constructor(private activity:ActivityService, private api: ApiService) { }
-  activityGenerator:boolean  = true
-
-  activities: [] = []
   ngOnInit(): void {
+    this.activity.getFavoritesSubject().subscribe((res:Activity[])=>  {
+      this.activities = res;
+    })
   }
-  showActivityGenerator(){
-    this.activityGenerator = !this.activityGenerator
+  ngOnDestroy(): void {
+      this.subscriptions.forEach((subscription: Subscription) => {
+        subscription.unsubscribe();
+      })
   }
+
   addFavorite(){
-    this.activity.addFavorite
+    this.activity.addFavorite(this.currentActivity)
+    this.btnDisable = true;
+    
   }
+
   randomActivity(){
-    this.api.getRandomActivity
+    this.subscriptions.push(this.api.getRandomActivity()
+    .subscribe((res:Activity)=> {
+      let isDouble: boolean = false; 
+      this.activities.forEach((activity:Activity)=> {
+        if(activity.key === res.key){
+          isDouble = true;
+        }
+      })
+      if(isDouble){
+        this.randomActivity()
+      }
+      else{
+        this.currentActivity = res; 
+        this.btnDisable = false;
+      }
+    }));
   }
 }
