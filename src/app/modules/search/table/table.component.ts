dc2855faa6514d11c063b3a01dc731bb5de8ee38
Giuseppe Marchesiello <juzeppemar90@gmail.com>
2022-06-13 23:08:47 +0200

    Challenge 2 Completed

--- src/app/modules/search/table/table.component.ts
+++ src/app/modules/search/table/table.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Gender } from 'src/app/models/gender';
+import { SearchService } from 'src/app/services/search.service';
 
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.scss']
 })
-export class TableComponent implements OnInit {
+export class TableComponent implements OnInit, OnDestroy {
+  searchHistory!: Gender[];
+  subscription!: Subscription;
 
-  constructor() { }
+  constructor(private searchService: SearchService) { }
 
   ngOnInit(): void {
+    this.subscription = this.searchService.getSearchHistorySubject()
+      .subscribe((res: Gender[]) => {
+        this.searchHistory = res;
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+  deleteSearch(id: number | undefined): void {
+    if (typeof (id) === 'number') {
+      this.searchService.deleteSearch(id);
+    }
+  }
+
+  getBackground(probability: number): string {
+    const prob: number = probability * 100;
+    if (prob <= 40) {
+      return 'red';
+    } else if (prob > 40 && prob < 70) {
+      return 'yellow';
+    } else {
+      return 'green';
+    }
   }
 
 }
