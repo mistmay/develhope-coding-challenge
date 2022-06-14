dc23d9d1d598b4ff3cc207180e07741ecce5d9c0
Biagio Spada <spa.biagio981@gmail.com>
2022-06-14 21:07:06 +0200

    challenge 3 tab implement

--- src/app/modules/cocktail/favorite-cocktails/favorite-cocktails.component.ts
+++ src/app/modules/cocktail/favorite-cocktails/favorite-cocktails.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Cocktail } from 'src/app/models/cocktail';
+import { CocktailService } from 'src/app/services/cocktail.service';
 @Component({
   selector: 'app-favorite-cocktails',
   templateUrl: './favorite-cocktails.component.html',
   styleUrls: ['./favorite-cocktails.component.scss']
 })
-export class FavoriteCocktailsComponent implements OnInit {
+export class FavoriteCocktailsComponent implements OnInit, OnDestroy{
 
-  constructor() { }
+  favorite!:Cocktail[]
+  subscriptions!:Subscription
+
+  constructor(private cocktailService: CocktailService) { }
 
   ngOnInit(): void {
+    this.subscriptions = this.cocktailService.getFavoritesSubject()
+    .subscribe((cocktail: Cocktail[]) => {
+      this.favorite = cocktail
+    })
+    
   }
 
+deleteFavorite(id:string) {
+  this.cocktailService.deleteFavorite(id)
+}
+
+
+ ngOnDestroy() {
+  this.subscriptions.unsubscribe()
+ }
 }
