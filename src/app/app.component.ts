818c4df9e9bd4e8517c286b021675f7231615c11
Giuseppe Marchesiello <juzeppemar90@gmail.com>
2022-06-14 09:30:19 +0200

    Add: challenge-3 form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Cocktail } from './models/cocktail';
+import { CocktailService } from './services/cocktail.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'coding-challenge';
+export class AppComponent implements OnInit {
+
+  constructor(private cocktailService: CocktailService) { }
+
+  ngOnInit(): void {
+    if (localStorage['favoriteCocktails']) {
+      const localStorageArray: Cocktail[] = JSON.parse(localStorage.getItem('favoriteCocktails') || "");
+      if (localStorageArray.length > 0) {
+        localStorageArray.forEach((cocktail: Cocktail) => {
+          this.cocktailService.addFavorite(cocktail);
+        });
+      }
+    }
+  }
+
 }
