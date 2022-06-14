5b94cad6ec9202ec44e6277657262f980e6726ae
Giuseppe Marchesiello <juzeppemar90@gmail.com>
2022-06-14 21:50:56 +0200

    Challenge 3 completed

--- src/app/modules/cocktail/favorite-cocktails/favorite-cocktails.component.ts
+++ src/app/modules/cocktail/favorite-cocktails/favorite-cocktails.component.ts
@@ -2,32 +2,31 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { Cocktail } from 'src/app/models/cocktail';
 import { CocktailService } from 'src/app/services/cocktail.service';
+
 @Component({
   selector: 'app-favorite-cocktails',
   templateUrl: './favorite-cocktails.component.html',
   styleUrls: ['./favorite-cocktails.component.scss']
 })
-export class FavoriteCocktailsComponent implements OnInit, OnDestroy{
+export class FavoriteCocktailsComponent implements OnInit, OnDestroy {
 
-  favorite!:Cocktail[]
-  subscriptions!:Subscription
+  favorite!: Cocktail[];
+  subscriptions!: Subscription;
 
   constructor(private cocktailService: CocktailService) { }
 
   ngOnInit(): void {
     this.subscriptions = this.cocktailService.getFavoritesSubject()
-    .subscribe((cocktail: Cocktail[]) => {
-      this.favorite = cocktail
-    })
-    
+      .subscribe((cocktail: Cocktail[]) => {
+        this.favorite = cocktail
+      });
   }
 
-deleteFavorite(id:string) {
-  this.cocktailService.deleteFavorite(id)
-}
+  deleteFavorite(id: string) {
+    this.cocktailService.deleteFavorite(id);
+  }
 
-
- ngOnDestroy() {
-  this.subscriptions.unsubscribe()
- }
+  ngOnDestroy() {
+    this.subscriptions.unsubscribe();
+  }
 }
